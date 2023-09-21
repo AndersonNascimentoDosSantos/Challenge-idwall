28475cc7a6a9da6f3d409e09876534f684409c8c
Douglas <douglas.s.santos1603@gmail.com>
2023-09-20 22:30:57 -0300

    removendo classes desnecessarias e atualizando dependencias do projeto

--- src/main/java/com/idwall/challengeapi/repositories/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.idwall.challengeapi.repositories;
-
-import com.idwall.challengeapi.entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<Person,Long> {
-}
