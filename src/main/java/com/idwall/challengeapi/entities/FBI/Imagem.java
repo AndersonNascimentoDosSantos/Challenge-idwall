e21da29f2fda4baa52e7930b054db76a1d4ff230
Anderson Nascimento dos Santos <tec.anderson.ns@gmail.com>
2023-09-24 13:32:10 -0300

    fix:relacionamento entre imagens e itens

--- src/main/java/com/idwall/challengeapi/entities/FBI/Imagem.java
+++ src/main/java/com/idwall/challengeapi/entities/FBI/Imagem.java
@@ -1,16 +1,22 @@
 package com.idwall.challengeapi.entities.FBI;
 
 import com.google.gson.annotations.SerializedName;
-import jakarta.persistence.*;
-import org.springframework.stereotype.Component;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
-@Component
+
 @Entity
 @Table(name = "T_FBI_IMAGES")
 public class Imagem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
     @SerializedName("large")
     private String largeURL;
 
@@ -21,7 +27,7 @@ public class Imagem {
     private String originalURL;
 
     @ManyToOne
-    @JoinColumn(name = "item_uid", referencedColumnName = "uid")
+    @JoinColumn(name = "item_id", referencedColumnName = "id")
     private Item item;
 
     // Adicione getters para os campos acima
--- src/main/java/com/idwall/challengeapi/entities/FBI/Item.java
+++ src/main/java/com/idwall/challengeapi/entities/FBI/Item.java
@@ -1,44 +1,52 @@
 package com.idwall.challengeapi.entities.FBI;
 
-import com.google.gson.annotations.SerializedName;
-import jakarta.persistence.*;
-import org.springframework.stereotype.Component;
-
-import java.util.ArrayList;
 import java.util.List;
+import com.google.gson.annotations.SerializedName;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
-@Component
+
 @Entity
 @Table(name = "T_FBI_ITEM")
 public class Item {
-
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @SerializedName("uid")
     private String uid;
+
     private String title;
+
     @SerializedName("scars_and_marks")
     private String scarsAndMarks;
+
+    @SerializedName("sex")
     private String sex;
+
+    @SerializedName("publication")
     private String publication;
+
     private String status;
 
-    public void setImages(ArrayList<Imagem> images) {
+    public void setImages(List<Imagem> images) {
         this.images = images;
     }
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "item")
+    @OneToMany(cascade = CascadeType.ALL)
     private List<Imagem> images;
 
     public String getStatus() {
         return status;
     }
 
-    public List<String> getImages() {
-        ArrayList<String> imgUrls = new ArrayList<>();
-        for (Imagem imagem : images) {
-            imgUrls.add(imagem.getLargeURL());
-            imgUrls.add(imagem.getOriginalURL());
-        }
-        return imgUrls;
+    public List<Imagem> getImages() {
+        return this.images;
     }
 
     public String getTitle() {
