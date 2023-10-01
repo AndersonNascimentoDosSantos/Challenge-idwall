a0122b9cf40f8a869dc1329049fa081ac2836a34
anderson nascimento dos santos <tec.anderson.ns@gmail.com>
2023-10-01 09:37:33 -0300

    refactor: adjust to receive the new paramenter

--- src/main/java/com/idwall/challengeapi/controllers/NoticesController.java
+++ src/main/java/com/idwall/challengeapi/controllers/NoticesController.java
@@ -1,17 +1,20 @@
 package com.idwall.challengeapi.controllers;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.idwall.challengeapi.entities.Interpol.Notice;
+import com.idwall.challengeapi.interfaces.InterpolParameters;
+import com.idwall.challengeapi.interfaces.QueryParameters;
+import com.idwall.challengeapi.services.InterpolService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.idwall.challengeapi.entities.Interpol.Notice;
-import com.idwall.challengeapi.services.InterpolService;
-
+import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Map;
-
-import jakarta.transaction.Transactional;
 
 /**
  * NoticesController
@@ -20,11 +23,21 @@ import jakarta.transaction.Transactional;
 public class NoticesController {
 	@Autowired
 	private InterpolService interpolService;
+	private final ModelMapper modelMapper;
+
+	public NoticesController(ModelMapper modelMapper) {
+		this.modelMapper = modelMapper;
+	}
+
 
 	@GetMapping("/coletar-dados-interpol")
 	@Transactional
 	public Notice getInterpolData(@RequestParam Map<String, String> queryParams) {
-		return interpolService.getInterpolData(queryParams);
+		Gson gson = new GsonBuilder().setLenient().create();
+		QueryParameters queryParameters = gson.fromJson(queryParams.toString(), QueryParameters.class);
+
+		InterpolParameters interpolParameters = modelMapper.map(queryParameters,InterpolParameters.class);
+		return interpolService.getInterpolData(interpolParameters);
 
 	}
 
